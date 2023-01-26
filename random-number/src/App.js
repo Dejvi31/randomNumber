2bcdbee9066006b3835760a18976d4d0e5d74a99
dejvi <dejviepadejvi@gmail.com>
2023-01-26 12:14:07 +0100

    shtuam filen

--- random-number/src/App.js
+++ random-number/src/App.js
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, {useState} from 'react'
+import './App.css'
 
 const App = () => {
+  const [minVal, setMinVal] = useState(0)
+  const [maxVal, setMaxVal] = useState(10)
+  const [randomNumber, setRandomNumber] = useState(5)
+ 
+  const handleRandom = () => {
+    setRandomNumber(Math.floor(Math.random() * (maxVal - minVal + 1) + minVal))
+  }
+
   return (
-    <div>App</div>
+    <div className='App'>
+      <div className='container'>
+        <div className='randomNum'>
+          <p>
+            Random number <span>{randomNumber}</span>
+          </p>
+        </div>
+        <div className='numContainer'>
+          <div>
+            <p>Min:</p>
+            <input type='number' value={minVal} onChange={e => setMinVal(+e.target.value)} />
+          </div>
+          <div>
+            <p>Max:</p>
+            <input type='number' value={maxVal} onChange={e => setMaxVal(+e.target.value)} />
+          </div>
+        </div>
+        <button onClick={handleRandom}>Get random number</button>
+      </div>
+    </div>
   )
 }
 
